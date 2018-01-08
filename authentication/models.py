6442b9404dc76aba6affbd5ab43b9982da854447
hchockarprasad <hchockarprasad@gmail.com>
2018-01-08 22:55:22 +0530

    create app authentication with base models

---
+++ authentication/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'tbl_menu'
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'tbl_role'
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+
+class Privilege(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.PROTECT)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    access = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'tbl_privilege'
+
+    def __str__(self):
+        return self.pk
+
+    def __unicode__(self):
+        return self.pk
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'tbl_user_profile'
+
+    def __str__(self):
+        return self.user.username
+
+    def __unicode__(self):
+        return self.user.username
