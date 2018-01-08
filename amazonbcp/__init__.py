bfba2d9a3a04325fe1c9feba8ea1c0b381f880c5
hchockarprasad <hchockarprasad@gmail.com>
2018-01-08 14:59:13 +0530

    add necessary project files

---
+++ amazonbcp/__init__.py
@@ -0,0 +1,12 @@
+r"""
+Amazon BCP
+"""
+
+__title__ = 'Amazon BCP'
+__version__ = '1.0.0'
+__author__ = 'hchockarprasad'
+__license__ = 'BSD 2-Clause'
+__copyright__ = 'Copyright 2018 hchockarprasad'
+
+# Version synonym
+VERSION = __version__
